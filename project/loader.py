0f105845d14bf4f2e065e8bb96c38d08bee1cae0
Andrey Lyakh <adelak@ukr.net>
2017-03-20 12:47:54 +0200

    fixed problem with encoding

--- project/loader.py
+++ project/loader.py
@@ -4,8 +4,13 @@ import pandas as pd
 from bs4 import BeautifulSoup
 import pprint
 from matplotlib import pyplot as plt
+from html5print import HTMLBeautifier
+import re
 
 pp = pprint.PrettyPrinter()
-f=open('source.html', 'r')
-soup = BeautifulSoup(f.read(),"html5lib")
-print soup.prettify()
+f=open('chart.htm', 'r')
+htmldata = f.read()
+soup = BeautifulSoup(htmldata, "html5lib")
+tags_a=soup.findAll('a',attrs={"href" : re.compile("^20")})
+print pp.pprint(tags_a)
+#print soup.prettify().encode('UTF-8')
