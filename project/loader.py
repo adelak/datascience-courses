266c3c553d25858d1975be475c5c9c890cbba192
Andrey Lyakh <adelak@ukr.net>
2017-03-21 01:45:41 +0200

    parse links

--- project/loader.py
+++ project/loader.py
@@ -11,6 +11,19 @@ pp = pprint.PrettyPrinter()
 f=open('chart.htm', 'r')
 htmldata = f.read()
 soup = BeautifulSoup(htmldata, "html5lib")
-tags_a=soup.findAll('a',attrs={"href" : re.compile("^20")})
-print pp.pprint(tags_a)
+#tags_a=[]
+links=[]
+for tag_a in soup.findAll('a',attrs={"href" : re.compile("^20")}):
+    links.append(tag_a['href'])
+
+#print pp.pprint(tags_a)
+l_df=pd.DataFrame(links)
+l_df.columns=['link']
+l_df['date']=l_df['link'].str.rsplit('-',6)
+#l_df['gender']=l_df['link'].str.rsplit('-',5)
+#l_df['tour']=l_df['link'].str.rsplit('-',4)
+#l_df['stage']=l_df['link'].str.rsplit('-',3)
+#l_df['player1']=l_df['link'].str.rsplit('-')[2]
+#l_df['player2']=l_df['link'].str.rsplit('-')[1]
+print l_df
 #print soup.prettify().encode('UTF-8')
